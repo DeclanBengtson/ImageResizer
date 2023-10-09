e8098264e242c1be4d03a996b501a2ebcaa4b8b2
Thomas Shields <n10994181@qut.edu.au>
2023-10-09 19:08:50 +1000

    Updated Comments

--- app.js
+++ app.js
@@ -1,50 +1,59 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+// Import necessary libraries and modules
+var createError = require('http-errors');  // For creating HTTP errors
+var express = require('express');  // Web framework
+var path = require('path');  // Node.js path module
+var cookieParser = require('cookie-parser');  // Middleware to parse cookies
+var logger = require('morgan');  // HTTP request logger middleware
 
+// Import route definitions
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+
+// Middleware for session handling
 const session = require("express-session");
 
+// Initialize the express application
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+// Set up the view engine for rendering templates
+app.set('views', path.join(__dirname, 'views'));  // Set the directory for the views
+app.set('view engine', 'hbs');  // Set the view engine to handlebars
 
-app.use(express.static('public')); // or the name of the directory containing your images
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// Set up middlewares
+app.use(express.static('public')); // Serve static files from the 'public' directory
+app.use(logger('dev'));  // Use the 'dev' log format for logging HTTP requests
+app.use(express.json());  // Parse JSON request bodies
+app.use(express.urlencoded({ extended: false }));  // Parse URL-encoded request bodies
+app.use(cookieParser());  // Use cookie parser to parse cookies
+app.use(express.static(path.join(__dirname, 'public')));  // Serve static files from the directory specified
 
+// Set up session middleware
 app.use(session({
-  secret: 'your-secret-key',
-  resave: false,
-  saveUninitialized: true,
-  cookie: { maxAge: 60000 * 30 }  // 30 minutes
+  secret: 'your-secret-key',  // Encryption key for the session
+  resave: false,  // Don't save session if unmodified
+  saveUninitialized: true,  // Save uninitialized sessions
+  cookie: { maxAge: 60000 * 30 }  // Set the cookie's expiration time to 30 minutes
 }));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// Use the imported routes
+app.use('/', indexRouter);  // Use the index route for the root path
+app.use('/users', usersRouter);  // Use the users route for the '/users' path
 
-// catch 404 and forward to error handler
+// Catch 404 errors and forward them to the error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
+// General error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+  // Set the local variables, only display errors in development mode
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
+  // Render the error page
   res.status(err.status || 500);
   res.render('error');
 });
 
+// Export the express app module to be used in other parts of the application
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,56 +1,60 @@
 var express = require('express');
-const sharp = require('sharp');
-const multer = require('multer');
-const storage = multer.memoryStorage(); // Store the file in memory
-const upload = multer({ storage: storage });
-const fs = require('fs').promises;
+const sharp = require('sharp');  // Image processing library
+const multer = require('multer');  // Middleware for handling multipart/form-data (file uploads)
+const storage = multer.memoryStorage(); // Configuring multer to store uploaded files in memory
+const upload = multer({ storage: storage });  // Initialize multer with memory storage
+const fs = require('fs').promises;  // File system promises API for asynchronous file operations
 
-var router = express.Router();
+var router = express.Router();  // Create a new router object
 
-/* GET home page. */
+/* Endpoint to render the home page */
 router.get('/', function(req, res, next) {
-  // If session does not have uploadedImages, initialize with default image
+  // If the session doesn't have uploaded images, initialize with a default image
   if (!req.session.uploadedImages) {
     req.session.uploadedImages = ['../images/default.jpg'];
   }
 
+  // Render the home page with title and uploaded images
   res.render('index', {
     title: 'Express',
     uploadedImages: req.session.uploadedImages,
   });
 });
 
-
+/* Endpoint to resize the uploaded image */
 router.post('/resize', upload.single('image'), async function(req, res) {
   try {
+    // If the session contains the default image, remove it before adding new images
     if (req.session.uploadedImages && req.session.uploadedImages[0] === '../images/default.jpg') {
       req.session.uploadedImages.shift();
     }
 
-    // Add the newly saved image to the session
+    // Add the new image path to the session
     if (!req.session.uploadedImages) {
       req.session.uploadedImages = [];
     }
     req.session.uploadedImages.push('../images/' + req.file.originalname);
 
-    let imageBuffer = req.file.buffer;
-    let width = parseInt(req.body.width);
-    let height = parseInt(req.body.height);
-    let imageType = req.body.imageType || 'jpeg';  // default to jpeg
-    let imageQuality = parseInt(req.body.imageQuality) || 90;  // default quality
-    let maintainAspectRatio = req.body.maintainAspectRatio === 'on';  // true if 'on', false otherwise
+    // Extract and configure image parameters for resizing
+    let imageBuffer = req.file.buffer;  // Buffer containing the uploaded image
+    let width = parseInt(req.body.width);  // Desired width for the resized image
+    let height = parseInt(req.body.height);  // Desired height for the resized image
+    let imageType = req.body.imageType || 'jpeg';  // Image type/format (default is jpeg)
+    let imageQuality = parseInt(req.body.imageQuality) || 90;  // Quality of the image (default is 90)
+    let maintainAspectRatio = req.body.maintainAspectRatio === 'on';  // Check if aspect ratio needs to be maintained
 
+    // Define the resize options
     let resizeOptions = {
       width: width,
-      fit: maintainAspectRatio ? sharp.fit.inside : sharp.fit.fill
+      fit: maintainAspectRatio ? sharp.fit.inside : sharp.fit.fill  // Resize strategy based on aspect ratio preference
     };
-
     if (height) {
       resizeOptions.height = height;
     }
 
+    // Use Sharp to process and resize the image
     let sharpInstance = sharp(imageBuffer).resize(resizeOptions);
-
+    // Configure image type and quality with Sharp
     if (imageType === 'jpeg') {
       sharpInstance = sharpInstance.jpeg({ quality: imageQuality });
     } else if (imageType === 'png') {
@@ -59,16 +63,18 @@ router.post('/resize', upload.single('image'), async function(req, res) {
       sharpInstance = sharpInstance.gif();
     }
 
-    let outputBuffer = await sharpInstance.toBuffer();
+    let outputBuffer = await sharpInstance.toBuffer();  // Get the resized image buffer
 
-    // Check if the client wants to download the image or not
+    // Check if the client wants the image as a download
     let download = req.body.download === 'on';
 
     if (download) {
+      // Set headers and send the resized image as a downloadable file
       res.setHeader('Content-Disposition', 'attachment; filename=resized-image.' + imageType);
       res.setHeader('Content-Type', req.file.mimetype);
       res.end(outputBuffer);
     } else {
+      // Otherwise, render the page with the resized image (encoded in Base64 format)
       let outputBase64 = `data:image/${imageType};base64,` + outputBuffer.toString('base64');
       res.render('index', {
         title: 'Cloud Resizer',
@@ -78,6 +84,7 @@ router.post('/resize', upload.single('image'), async function(req, res) {
     }
 
   } catch (error) {
+    // In case of errors, render the home page with an error message
     res.render('index', {
       title: 'Cloud Resizer',
       error: error.message
@@ -85,5 +92,5 @@ router.post('/resize', upload.single('image'), async function(req, res) {
   }
 });
 
-
+// Export the router module to be used in other parts of the application
 module.exports = router;
